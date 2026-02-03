7c3c32f26eba05ef36f004726a4ce7253a5a4e02
koyamaM25 <masamasakotsubu@gmail.com>
2026-02-03 23:26:01 +0900

    knock85修正

--- 9章/code/knock85.py
+++ 9章/code/knock85.py
@@ -1,14 +1,12 @@
-from transformers import AutoTokenizer, AutoModel
-import torch
-import torch.nn.functional as F
+from transformers import AutoTokenizer
 import zipfile
 import pandas as pd
+import pickle
 import os
 
 SST_DATA = "/home/koyama/nlp-100knocks/7章/code/SST-2.zip"
 OUTPUT_PATH = "/home/koyama/nlp-100knocks/9章/out"
 os.makedirs(OUTPUT_PATH, exist_ok=True)
-OUTPUT_FILE = os.path.join(OUTPUT_PATH, 'out_85.txt')
 
 with zipfile.ZipFile(SST_DATA, "r") as z:
     with z.open('SST-2/train.tsv') as f:
@@ -16,32 +14,17 @@ with zipfile.ZipFile(SST_DATA, "r") as z:
     with z.open('SST-2/dev.tsv') as f:
         df_dev = pd.read_csv(f, sep='\t')
 
-print(df_dev[:1])
-
 tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
-model = AutoModel.from_pretrained("bert-base-uncased")
 
-def create_ids(df):
-    df['sentence'] = tokenizer(df['sentence'], return_tensors='pt')
-    df['sentence'] = tokenizer.tokenize(df['sentence'])
+def tokenize_texts(df):
+    df['tokens'] = df['sentence'].apply(tokenizer.tokenize)
+    return df
 
-create_ids(df_dev)
-create_ids(df_train)
+df_dev = tokenize_texts(df_dev)
+df_train = tokenize_texts(df_train)
+print(df_dev[:1])
+print(df_train[:1])
 
-# embs = []
-# # 単語→ID
-# for text in texts:
-#     inputs = tokenizer(text, return_tensors='pt')
-#     # 推論
-#     with torch.no_grad():
-#         outputs = model(**inputs)
-#         embs.append(outputs.last_hidden_state[0,0,:])
-#         # dim=0 で「単語数」の方向につぶして平均をとる
-#         token_mean = outputs.last_hidden_state[0].mean(dim=0)
-#         embs.append(token_mean)
-
-# with open(OUTPUT_FILE, 'w', encoding='UTF-8') as f:
-#     for i in range(len(texts)):
-#         for j in range(i + 1, len(texts)):
-#             sim = F.cosine_similarity(embs[i], embs[j], dim=0)
-#             f.write(f"{texts[i]}\t{texts[j]}\t{sim.item():.4f}\n")
+# 保存
+df_train.to_pickle(os.path.join(OUTPUT_PATH, 'out_85_train.pkl'))
+df_dev.to_pickle(os.path.join(OUTPUT_PATH, 'out_85_dev.pkl'))
